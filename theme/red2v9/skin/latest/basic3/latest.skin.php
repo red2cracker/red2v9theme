e5cd5c5e179ef45bc44f29126ee3274f44f99c3a
leezche <red2cracker@gmail.com>
2018-01-10 11:09:07 +0900

    최신글 스킨 3 업로드.

---
+++ theme/red2v9/skin/latest/basic3/latest.skin.php
@@ -0,0 +1,45 @@
+<?php
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+// add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
+add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 0);
+?>
+
+<!-- <?php echo $bo_subject; ?> 최신글 시작 { -->
+<!-- style.css에 padding: 0 을 주면 먹히지 않는다.
+그래서 container-padding 을 추가했다. CSS 순서 때문인듯. -->
+<article class="col-md-6 latest">
+    <h2><a href="<?php echo G5_BBS_URL ?>/board.php?bo_table=<?php echo $bo_table ?>"><?php echo $bo_subject; ?></a></h2>
+    <ul>
+    <?php for ($i=0; $i<count($list); $i++) {  ?>
+        <li>
+            <?php
+            //echo $list[$i]['icon_reply']." ";
+            echo "<a href=\"".$list[$i]['href']."\">";
+            if ($list[$i]['is_notice'])
+                echo "<strong>".$list[$i]['subject']."</strong>";
+            else
+                echo $list[$i]['subject'];
+
+            if ($list[$i]['comment_cnt'])
+                echo $list[$i]['comment_cnt'];
+
+            echo "</a>";
+
+            // if ($list[$i]['link']['count']) { echo "[{$list[$i]['link']['count']}]"; }
+            // if ($list[$i]['file']['count']) { echo "<{$list[$i]['file']['count']}>"; }
+
+            if (isset($list[$i]['icon_new'])) echo " " . $list[$i]['icon_new'];
+            // if (isset($list[$i]['icon_hot'])) echo " " . $list[$i]['icon_hot'];
+            if (isset($list[$i]['icon_file'])) echo " " . $list[$i]['icon_file'];
+            if (isset($list[$i]['icon_link'])) echo " " . $list[$i]['icon_link'];
+            if (isset($list[$i]['icon_secret'])) echo " " . $list[$i]['icon_secret'];
+             ?>
+        </li>
+    <?php }  ?>
+    <?php if (count($list) == 0) { //게시물이 없을 때  ?>
+    <li>게시물이 없습니다.</li>
+    <?php }  ?>
+    </ul>
+</article>
+<!-- } <?php echo $bo_subject; ?> 최신글 끝 -->
