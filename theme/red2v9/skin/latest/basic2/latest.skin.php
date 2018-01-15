bef4c55e88aa4064ab073fa02bfca8dbc87fefa3
leezche <red2cracker@gmail.com>
2018-01-15 12:37:29 +0900

    최신글 스킨 링크 아이콘 제거.

--- theme/red2v9/skin/latest/basic2/latest.skin.php
+++ theme/red2v9/skin/latest/basic2/latest.skin.php
@@ -32,7 +32,7 @@ add_stylesheet('<link rel="stylesheet" href="'.$latest_skin_url.'/style.css">', 
             if (isset($list[$i]['icon_new'])) echo " " . $list[$i]['icon_new'];
             // if (isset($list[$i]['icon_hot'])) echo " " . $list[$i]['icon_hot'];
             if (isset($list[$i]['icon_file'])) echo " " . $list[$i]['icon_file'];
-            if (isset($list[$i]['icon_link'])) echo " " . $list[$i]['icon_link'];
+            //if (isset($list[$i]['icon_link'])) echo " " . $list[$i]['icon_link'];
             if (isset($list[$i]['icon_secret'])) echo " " . $list[$i]['icon_secret'];
              ?>
         </li>
